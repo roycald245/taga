234a54fc103985130ffb92a67ee0b5fbf45a1fba
roy <roycald123@gmail.com>
2022-11-22 12:07:22 +0200

    fix

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 from pyspark.sql import SparkSession
 
+from Model import Model
 from workflow import execute_workflow
 
 if __name__ == '__main__':
     spark = SparkSession.builder.master('local').appName('taga').getOrCreate()
 
     df = spark.read.csv('./test_table.csv', header=True)
-    df, model = execute_workflow(df, {})
+    df, model = execute_workflow(df, Model({
+        'fullest_name': {
+            'references': [
+                {
+                    'type': 'column',
+                    'value': 'fullest_name1'
+                },
+                {
+                    'type': 'column',
+                    'value': 'fullest_name2'
+                }
+            ]
+        },
+        'imei': {
+            'references': [
+                {'type': 'column', 'value': 'imei'}
+            ]
+        },
+        'id': {
+            'references': [
+                {'type': 'column', 'value': 'id'}
+            ]
+        },
+        'pstn': {
+            'references': [
+                {'type': 'column', 'value': 'pstn'}
+            ]
+        }
+    }))
     df.show()
-
--- steps/ConcatBdt.py
+++ steps/ConcatBdt.py
@@ -9,13 +9,13 @@ class ConcatBdt(IStep):
 
     def process(self, df):
         for bdt_name, bdt_body in self.model.items():
-            if len(bdt_body.references) > 1:
+            if len(bdt_body.get('references')) > 1:
                 concat_columns = []
-                for ref in bdt_body.references:
-                    if ref.type == 'column':
-                        concat_columns.append(F.col(ref.value))
+                for ref in bdt_body.get('references'):
+                    if ref.get('type') == 'column':
+                        concat_columns.append(F.col(ref.get('value')))
                     else:
-                        concat_columns.append(F.lit(ref.value))
+                        concat_columns.append(F.lit(ref.get('value')))
 
                 df = df.withColumn(bdt_name, F.concat(*concat_columns))
 
