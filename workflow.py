255f8839e6e143920d61405f723f5b3c815d6804
roy <roycald123@gmail.com>
2022-11-28 18:20:17 +0200

    remove ConditionalTagging.py

--- workflow.py
+++ workflow.py
@@ -1,4 +1,3 @@
-from steps.ConditionalTagging import ConditionalTagging
 from steps.AddConsts import AddConsts
 from steps.ConcatBdt import ConcatBdt
 
@@ -6,5 +5,4 @@ from steps.ConcatBdt import ConcatBdt
 def execute_workflow(df, model):
     df, model = ConcatBdt(model).process(df)
     df, model = AddConsts(model).process(df)
-    df, model = ConditionalTagging(model).process(df)
     return df, model
